72b43772d646090637281ea8349583fadbc34f3c
Antoine GRICOURT <agricourt@kiloutou.fr>
2024-08-01 14:40:16 +0200

    feat(log-metric-filter): add output for namespace

--- modules/log-metric-filter/outputs.tf
+++ modules/log-metric-filter/outputs.tf
@@ -2,3 +2,8 @@ output "cloudwatch_log_metric_filter_id" {
   description = "The name of the metric filter"
   value       = try(aws_cloudwatch_log_metric_filter.this[0].id, "")
 }
+
+output "cloudwatch_log_metric_filter_namespace" {
+  description = "The namespace of the metric filter"
+  value       = try(aws_cloudwatch_log_metric_filter.this[0].namespace, "")
+}
